59be504a8dd82dbbf8de7c34842bbd986217a7dc
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2025-03-24 21:59:14 -0300

    Fixing LocalStorage4Pascal
    Trying to amke LocalStorage4Pascal compatible for Lazarus

--- src/LocalStorage4Pascal.pas
+++ src/LocalStorage4Pascal.pas
@@ -17,7 +17,6 @@ uses
   {$ENDIF};
 
 type
-
   iLocalStorage4Pascal = interface
     ['{076E3E57-880E-4744-B3B4-2514FC4F79C2}']
     {$region 'SetValue methods'}
@@ -37,14 +36,14 @@ type
     function GetDouble(const Key: string; const Default: Double = 0.0): Double;
     function GetJSONObject(const Key: string; const Default: TJSONObject = nil): TJSONObject;
     function GetJSONArray(const Key: string; const Default: TJSONArray = nil): TJSONArray;
-    function GetInt64(const key: string; const Default: Int64): Int64;
+    function GetInt64(const Key: string; const Default: Int64 = 0): Int64;
     {$endregion}
 
     function RemoveValue(const Key: string): Boolean;
     function KeyExists(const Key: string): Boolean;
     function Clear: Boolean;
 
-    function Version : string;
+    function Version: string;
   end;
 
   TLocalStorage4Pascal = class(TInterfacedObject, iLocalStorage4Pascal)
@@ -54,9 +53,11 @@ type
     function GetStoragePath(const FileName: string = 'localstorage.json'): string;
     procedure LoadFromFile;
     procedure SaveToFile;
-    function GetValue<T>(const key: string; Default: T): T;
-    function SetValue<T>(const key: string; const Value: T): T; overload;
-    function AsType<T>(JsonData: TJsonData): T;
+    function GetValue<T>(const Key: string; Default: T): T;
+    function SetValue<T>(const Key: string; const Value: T): T; overload;
+    {$IFDEF FPC}
+    function AsType<T>(JsonData: TJSONData): T;
+    {$ENDIF}
   public
     constructor Create(const FileName: string = 'LocalStorage.json');
     destructor Destroy; override;
@@ -75,20 +76,20 @@ type
     function GetDouble(const Key: string; const Default: Double = 0.0): Double;
     function GetJSONObject(const Key: string; const Default: TJSONObject = nil): TJSONObject;
     function GetJSONArray(const Key: string; const Default: TJSONArray = nil): TJSONArray;
-    function GetInt64(const key: string; const Default: Int64): Int64;
+    function GetInt64(const Key: string; const Default: Int64 = 0): Int64;
 
     function RemoveValue(const Key: string): Boolean;
     function KeyExists(const Key: string): Boolean;
     function Clear: Boolean;
 
-    function Version : string;
+    function Version: string;
   end;
 
-  function InitLocalStorage4Pascal(const FileName: string = 'localstorage.json') : iLocalStorage4Pascal;
+  function InitLocalStorage4Pascal(const FileName: string = 'localstorage.json'): iLocalStorage4Pascal;
 
 var
-  LocalStorage4Delphi  : iLocalStorage4Pascal;
-  LocalStorage4Lazarus : iLocalStorage4Pascal;
+  LocalStorage4Delphi: iLocalStorage4Pascal;
+  LocalStorage4Lazarus: iLocalStorage4Pascal;
 
 const
   LocaStorage4Pascal_Version = '1.0.0';
@@ -97,118 +98,165 @@ implementation
 
 uses
   {$IFDEF FPC}
-  typinfo;
+  TypInfo,
+  RTTI,
+  LazUTF8
   {$ELSE}
   System.IOUtils,
   System.TypInfo,
-  System.Rtti;
-  {$ENDIF}
+  System.Rtti
+  {$ENDIF};
 
-function InitLocalStorage4Pascal(const FileName: string = 'localstorage.json') : iLocalStorage4Pascal;
+function InitLocalStorage4Pascal(const FileName: string = 'localstorage.json'): iLocalStorage4Pascal;
 begin
   {$IFDEF FPC}
-  if (not Assigned(LocalStorage4Lazarus)) then
-   LocalStorage4Lazarus := TLocalStorage4Pascal.New(FileName);
-
+  if not Assigned(LocalStorage4Lazarus) then
+    LocalStorage4Lazarus := TLocalStorage4Pascal.New(FileName);
   Result := LocalStorage4Lazarus;
-  {$else}
-  if (not Assigned(LocalStorage4Delphi)) then
-   LocalStorage4Delphi := TLocalStorage4Pascal.New(FileName);
-
+  {$ELSE}
+  if not Assigned(LocalStorage4Delphi) then
+    LocalStorage4Delphi := TLocalStorage4Pascal.New(FileName);
   Result := LocalStorage4Delphi;
-  {$endif}
+  {$ENDIF}
 end;
 
 { TLocalStorage4Pascal }
 
-function TLocalStorage4Pascal.AsType<T>(JsonData: TJsonData): T;
+{$IFDEF FPC}
+function TLocalStorage4Pascal.AsType<T>(JsonData: TJSONData): T;
 begin
   if JsonData = nil then
     raise Exception.Create('Dados JSON não encontrados');
+
   case PTypeInfo(TypeInfo(T))^.Kind of
     tkInteger:
-      Result := T(JsonData.AsInteger);
-    tkInt64:  // Adicionado suporte para Int64
-      Result := T(JsonData.AsInt64);
+      Result := TypeCast<T>(JsonData.AsInteger);
+    tkInt64:
+      Result := TypeCast<T>(JsonData.AsInt64);
     tkFloat:
-      Result := T(JsonData.AsFloat);
+      Result := TypeCast<T>(JsonData.AsFloat);
     tkUString, tkString:
-      Result := T(JsonData.AsString);
-    tkBool:
-      Result := T(JsonData.AsBoolean);
+      Result := TypeCast<T>(JsonData.AsString);
+    tkEnumeration:
+      if TypeInfo(T) = TypeInfo(Boolean) then
+        Result := TypeCast<T>(JsonData.AsBoolean)
+      else
+        raise Exception.Create('Tipo genérico não suportado: ' + PTypeInfo(TypeInfo(T))^.Name);
     else
-      raise Exception.Create('Tipo genérico não suportado');
+      raise Exception.Create('Tipo genérico não suportado: ' + PTypeInfo(TypeInfo(T))^.Name);
   end;
 end;
 
-function TLocalStorage4Pascal.GetValue<T>(const key: string; Default: T): T;
+// Função auxiliar para cast genérico
+function TypeCast<T>: T; inline;
+begin
+  Result := T(Value);
+end;
+{$ENDIF}
+
+function TLocalStorage4Pascal.GetValue<T>(const Key: string; Default: T): T;
+{$IFDEF FPC}
 var
-  JsonData: TJsonData;
+  JsonData: TJSONData;
+{$ENDIF}
 begin
   {$IFNDEF FPC}
   if FStorage.FindValue(Key) <> nil then
-    Result := FStorage.Values[key].AsType<T>
+    Result := FStorage.Values[Key].AsType<T>
   else
-   begin
-    SetValue(key, Default);
+  begin
+    SetValue(Key, Default);
     Result := Default;
-   end;
+  end;
   {$ELSE}
-    JsonData := FStorage.Find(Key);
-    if JsonData <> nil then
-      Result := AsType<T>(JsonData)
-    else
-    begin
-      SetValue(Key, Default);
-      Result := Default;
-    end;
+  JsonData := FStorage.Find(Key);
+  if JsonData <> nil then
+    Result := AsType<T>(JsonData)
+  else
+  begin
+    SetValue(Key, Default);
+    Result := Default;
+  end;
   {$ENDIF}
 end;
 
-procedure TLocalStorage4Pascal.SetValue(const Key: string; const Value: Int64);
-begin
-  //SetValue<Int64>(key, Value);
-end;
-
-function TLocalStorage4Pascal.SetValue<T>(const key: string; const Value: T): T;
+function TLocalStorage4Pascal.SetValue<T>(const Key: string; const Value: T): T;
 var
   StoredValue: TValue;
 begin
-  FStorage.RemovePair(key).Free;
+  {$IFDEF FPC}
+  FStorage.Delete(Key);
 
   StoredValue := TValue.From<T>(Value);
 
   if StoredValue.IsObject then
-    begin
-      if StoredValue.AsObject is TJSONObject then
-        FStorage.AddPair(key, TJSONObject(StoredValue.AsObject).Clone as TJSONObject)
-      else if StoredValue.AsObject is TJSONArray then
-        FStorage.AddPair(key, TJSONArray(StoredValue.AsObject).Clone as TJSONArray);
-    end
-   else
-    begin
-
-      case PTypeInfo(TypeInfo(T))^.Kind of
-        tkInteger:
-          FStorage.AddPair(key, StoredValue.AsInteger);
-        tkInt64:
-          FStorage.AddPair(key, StoredValue.AsInt64);
-        tkFloat:
-          FStorage.AddPair(key, StoredValue.AsExtended);
-        tkUString, tkString, tkLString:
-          FStorage.AddPair(key, StoredValue.AsString);
-        tkWString:
-          FStorage.AddPair(key, StoredValue.AsString);
-        tkEnumeration:
-          if TypeInfo(T) = TypeInfo(Boolean) then
-            FStorage.AddPair(key, StoredValue.AsBoolean);
+  begin
+    if StoredValue.AsObject is TJSONObject then
+      FStorage.Add(Key, TJSONObject(StoredValue.AsObject).Clone as TJSONObject)
+    else if StoredValue.AsObject is TJSONArray then
+      FStorage.Add(Key, TJSONArray(StoredValue.AsObject).Clone as TJSONArray);
+  end
+  else
+  begin
+    case PTypeInfo(TypeInfo(T))^.Kind of
+      tkInteger:
+        FStorage.Add(Key, StoredValue.AsInteger);
+      tkInt64:
+        FStorage.Add(Key, StoredValue.AsInt64);
+      tkFloat:
+        FStorage.Add(Key, StoredValue.AsExtended);
+      tkUString, tkString, tkLString:
+        FStorage.Add(Key, StoredValue.AsString);
+      tkWString:
+        FStorage.Add(Key, StoredValue.AsString);
+      tkEnumeration:
+        if TypeInfo(T) = TypeInfo(Boolean) then
+          FStorage.Add(Key, StoredValue.AsBoolean)
+        else
+          raise Exception.CreateFmt('Tipo não suportado: %s', [PTypeInfo(TypeInfo(T))^.Name]);
       else
         raise Exception.CreateFmt('Tipo não suportado: %s', [PTypeInfo(TypeInfo(T))^.Name]);
-      end;
     end;
+  end;
 
-    SaveToFile;
-    Result := T(value);
+  SaveToFile;
+  Result := Value; // Ajustado de T(Value) para Value
+  {$ELSE}
+  FStorage.RemovePair(Key).Free;
+
+  StoredValue := TValue.From<T>(Value);
+
+  if StoredValue.IsObject then
+  begin
+    if StoredValue.AsObject is TJSONObject then
+      FStorage.AddPair(Key, TJSONObject(StoredValue.AsObject).Clone as TJSONObject)
+    else if StoredValue.AsObject is TJSONArray then
+      FStorage.AddPair(Key, TJSONArray(StoredValue.AsObject).Clone as TJSONArray);
+  end
+  else
+  begin
+    case PTypeInfo(TypeInfo(T))^.Kind of
+      tkInteger:
+        FStorage.AddPair(Key, StoredValue.AsInteger);
+      tkInt64:
+        FStorage.AddPair(Key, StoredValue.AsInt64);
+      tkFloat:
+        FStorage.AddPair(Key, StoredValue.AsExtended);
+      tkUString, tkString, tkLString:
+        FStorage.AddPair(Key, StoredValue.AsString);
+      tkWString:
+        FStorage.AddPair(Key, StoredValue.AsString);
+      tkEnumeration:
+        if TypeInfo(T) = TypeInfo(Boolean) then
+          FStorage.AddPair(Key, StoredValue.AsBoolean);
+      else
+        raise Exception.CreateFmt('Tipo não suportado: %s', [PTypeInfo(TypeInfo(T))^.Name]);
+    end;
+  end;
+
+  SaveToFile;
+  Result := T(Value);
+  {$ENDIF}
 end;
 
 function TLocalStorage4Pascal.Version: string;
@@ -221,18 +269,57 @@ begin
   {$IFDEF MSWINDOWS}
   Result := IncludeTrailingPathDelimiter(ExtractFilePath(ParamStr(0))) + FileName;
   {$ELSE}
+  {$IFDEF FPC}
+  Result := IncludeTrailingPathDelimiter(GetUserDir) + FileName; // Substituí TPath.GetHomePath
+  {$ELSE}
   Result := IncludeTrailingPathDelimiter(TPath.GetHomePath) + FileName;
+  {$ENDIF}
   {$ENDIF}
 end;
 
 procedure TLocalStorage4Pascal.LoadFromFile;
 var
   JSONStr: string;
-  FileStream: TStreamReader;
+  FileStream: {$IFDEF FPC}TFileStream{$ELSE}TStreamReader{$ENDIF};
+  {$IFDEF FPC}StringStream: TStringStream;{$ENDIF}
+  {$IFDEF FPC}JSONData: TJSONData;{$ENDIF}
   JSONObj: TJSONObject;
 begin
   if not FileExists(FFilePath) then Exit;
 
+  {$IFDEF FPC}
+  try
+    FileStream := TFileStream.Create(FFilePath, fmOpenRead or fmShareDenyWrite);
+    try
+      StringStream := TStringStream.Create('');
+      try
+        StringStream.CopyFrom(FileStream, FileStream.Size);
+        JSONStr := StringStream.DataString;
+      finally
+        StringStream.Free;
+      end;
+    finally
+      FileStream.Free;
+    end;
+
+    if Trim(JSONStr) = '' then Exit;
+
+    JSONData := GetJSON(JSONStr);
+    if Assigned(JSONData) and (JSONData.JSONType = jtObject) then
+    begin
+      JSONObj := JSONData as TJSONObject;
+      FStorage.Free;
+      FStorage := JSONObj;
+    end
+    else
+    begin
+      JSONData.Free; // Libera se não for um objeto válido
+    end;
+  except
+    on E: Exception do
+      Writeln('Erro ao carregar o arquivo: ', E.Message); // Tratamento básico de erro
+  end;
+  {$ELSE}
   try
     FileStream := TStreamReader.Create(FFilePath, TEncoding.UTF8);
     try
@@ -252,13 +339,38 @@ begin
   except
     // Tratamento de erro opcional
   end;
+  {$ENDIF}
 end;
 
 procedure TLocalStorage4Pascal.SaveToFile;
 var
   JSONStr: string;
+  {$IFNDEF FPC}
   FileStream: TStreamWriter;
+  {$ELSE}
+  FileStream: TFileStream;
+  StringStream: TStringStream;
+  {$ENDIF}
 begin
+  {$IFDEF FPC}
+  JSONStr := FStorage.FormatJSON; // Substituímos ToJSON por FormatJSON
+  try
+    FileStream := TFileStream.Create(FFilePath, fmCreate or fmShareDenyRead);
+    try
+      StringStream := TStringStream.Create(JSONStr);
+      try
+        FileStream.CopyFrom(StringStream, StringStream.Size);
+      finally
+        StringStream.Free;
+      end;
+    finally
+      FileStream.Free;
+    end;
+  except
+    on E: Exception do
+      Writeln('Erro ao salvar o arquivo: ', E.Message); // Tratamento básico de erro
+  end;
+  {$ELSE}
   JSONStr := FStorage.ToJSON;
   try
     FileStream := TStreamWriter.Create(FFilePath, False, TEncoding.UTF8);
@@ -270,6 +382,7 @@ begin
   except
     // Tratamento de erro opcional
   end;
+  {$ENDIF}
 end;
 
 constructor TLocalStorage4Pascal.Create(const FileName: string);
@@ -280,22 +393,8 @@ begin
 end;
 
 destructor TLocalStorage4Pascal.Destroy;
-var
- i, a: integer;
- l : TJSONObject;
 begin
   SaveToFile;
-//  for I := 0 to FStorage.Count - 1 do
-//   begin
-//    if(FStorage.Pairs[i].JsonValue is TJSONObject)then
-//     begin
-//     L := FStorage.Pairs[i].JsonValue as TJSONObject;
-//     for a := 0 to L.Count - 1 do
-//       L.RemovePair(l.Pairs[a].JsonString.ToString).Free;
-//     end;
-//
-//    FStorage.RemovePair(FStorage.Pairs[i].JsonString.ToString).Free;
-//   end;
   FreeAndNil(FStorage);
   inherited;
 end;
@@ -312,7 +411,11 @@ begin
   Result := False;
   try
     for i := FStorage.Count - 1 downto 0 do
-      FStorage.RemovePair(FStorage.Pairs[i].JsonString.Value).Free; // Libera cada item
+      {$IFNDEF FPC}
+      FStorage.RemovePair(FStorage.Pairs[i].JsonString.Value).Free // Libera cada item
+      {$ELSE}
+      FStorage.Delete(i)
+      {$ENDIF};
     SaveToFile;
     Result := True;
   except
@@ -320,95 +423,161 @@ begin
   end;
 end;
 
-function TLocalStorage4Pascal.GetBoolean(const Key: string;const Default: Boolean): Boolean;
+function TLocalStorage4Pascal.GetBoolean(const Key: string; const Default: Boolean): Boolean;
 begin
-  Result := GetValue<Boolean>(key, Default);
+  Result := GetValue<Boolean>(Key, Default);
 end;
 
-function TLocalStorage4Pascal.GetDouble(const Key: string;const Default: Double): Double;
+function TLocalStorage4Pascal.GetDouble(const Key: string; const Default: Double): Double;
 begin
-  Result := GetValue<Double>(key, Default);
+  Result := GetValue<Double>(Key, Default);
 end;
 
-function TLocalStorage4Pascal.GetInt64(const key: string;const Default: Int64): Int64;
+function TLocalStorage4Pascal.GetInt64(const Key: string; const Default: Int64): Int64;
 begin
-  Result := GetValue<Int64>(key, Default);
+  Result := GetValue<Int64>(Key, Default);
 end;
 
-function TLocalStorage4Pascal.GetInteger(const Key: string;const Default: Integer): Integer;
+function TLocalStorage4Pascal.GetInteger(const Key: string; const Default: Integer): Integer;
 begin
-  Result := GetValue<integer>(key, Default);
+  Result := GetValue<Integer>(Key, Default);
 end;
 
-function TLocalStorage4Pascal.GetJSONArray(const Key: string;const Default: TJSONArray): TJSONArray;
+function TLocalStorage4Pascal.GetJSONArray(const Key: string; const Default: TJSONArray): TJSONArray;
+var
+  Value: TJSONArray;
 begin
-  Result := GetValue<TJSONArray>(key, Default).Clone as TJSONArray;
-
-  Default.Free;
+  Value := GetValue<TJSONArray>(Key, Default);
+  if Assigned(Value) then
+    Result := Value.Clone as TJSONArray
+  else
+    Result := nil;
+  {$IFNDEF FPC}
+  Default.Free; // Apenas no Delphi, pois Default é passado como const e pode ser liberado
+  {$ENDIF}
 end;
 
-function TLocalStorage4Pascal.GetJSONObject(const Key: string;const Default: TJSONObject): TJSONObject;
+function TLocalStorage4Pascal.GetJSONObject(const Key: string; const Default: TJSONObject): TJSONObject;
+var
+  Value: TJSONObject;
 begin
-  Result := GetValue<TJSONObject>(key, Default).Clone as TJSONObject;
-
-  Default.Free;
+  Value := GetValue<TJSONObject>(Key, Default);
+  if Assigned(Value) then
+    Result := Value.Clone as TJSONObject
+  else
+    Result := nil;
+  {$IFNDEF FPC}
+  Default.Free; // Apenas no Delphi, pois Default é passado como const e pode ser liberado
+  {$ENDIF}
 end;
 
-function TLocalStorage4Pascal.GetString(const Key, Default: string): string;
+function TLocalStorage4Pascal.GetString(const Key: string; const Default: string): string;
 begin
-  Result := GetValue<string>(key, Default);
+  Result := GetValue<string>(Key, Default);
 end;
 
 function TLocalStorage4Pascal.KeyExists(const Key: string): Boolean;
 begin
-  Result := assigned(FStorage.FindValue(key));
+  {$IFNDEF FPC}
+  Result := Assigned(FStorage.FindValue(Key));
+  {$ELSE}
+  Result := Assigned(FStorage.Find(Key));
+  {$ENDIF}
 end;
 
 function TLocalStorage4Pascal.RemoveValue(const Key: string): Boolean;
 var
+  {$IFNDEF FPC}
   Pair: TJSONPair;
+  {$ELSE}
+  Index: Integer;
+  {$ENDIF}
 begin
+  {$IFNDEF FPC}
   Pair := FStorage.RemovePair(Key);
-
   Result := Assigned(Pair);
-
   if Result then
-    Pair.Free;  // Libera o par removido para evitar vazamento
+    Pair.Free; // Libera o par removido para evitar vazamento
+  {$ELSE}
+  Index := FStorage.IndexOfName(Key);
+  Result := Index >= 0;
+  if Result then
+  begin
+    FStorage.Delete(Index);
+    SaveToFile; // Necessário no Lazarus para persistir a remoção
+  end;
+  {$ENDIF}
 end;
 
-procedure TLocalStorage4Pascal.SetValue(const Key: string;const Value: Integer);
+procedure TLocalStorage4Pascal.SetValue(const Key: string; const Value: Integer);
 begin
-  SetValue<integer>(key, value);
+  {$IFNDEF FPC}
+  SetValue<Integer>(Key, Value);
+  {$ELSE}
+  SetValue(Key, Value);
+  {$ENDIF}
 end;
 
 procedure TLocalStorage4Pascal.SetValue(const Key, Value: string);
 begin
-  SetValue<string>(key, value);
+  {$IFNDEF FPC}
+  SetValue<string>(Key, Value);
+  {$ELSE}
+  SetValue(Key, Value);
+  {$ENDIF}
 end;
 
-procedure TLocalStorage4Pascal.SetValue(const Key: string;const Value: TJSONArray);
+procedure TLocalStorage4Pascal.SetValue(const Key: string; const Value: TJSONArray);
 begin
-  SetValue<TJSONArray>(key, value);
+  {$IFNDEF FPC}
+  SetValue<TJSONArray>(Key, Value);
+  {$ELSE}
+  SetValue(Key, Value);
+  {$ENDIF}
 
-  Value.Free;
+  {$IFNDEF FPC}
+  Value.Free; // Apenas no Delphi, pois Value é passado como const e pode ser liberado
+  {$ENDIF}
 end;
 
-procedure TLocalStorage4Pascal.SetValue(const Key: string;const Value: TJSONObject);
+procedure TLocalStorage4Pascal.SetValue(const Key: string; const Value: TJSONObject);
 begin
-  SetValue<TJSONObject>(key, value);
+  {$IFNDEF FPC}
+  SetValue<TJSONObject>(Key, Value);
+  {$ELSE}
+  SetValue(Key, Value);
+  {$ENDIF}
 
-  Value.Free;
+  {$IFNDEF FPC}
+  Value.Free; // Apenas no Delphi, pois Value é passado como const e pode ser liberado
+  {$ENDIF}
 end;
 
 procedure TLocalStorage4Pascal.SetValue(const Key: string; const Value: Double);
 begin
-  SetValue<double>(key, value);
+  {$IFNDEF FPC}
+  SetValue<Double>(Key, Value);
+  {$ELSE}
+  SetValue(Key, Value);
+  {$ENDIF}
 end;
 
-procedure TLocalStorage4Pascal.SetValue(const Key: string;const Value: Boolean);
+procedure TLocalStorage4Pascal.SetValue(const Key: string; const Value: Boolean);
 begin
-  SetValue<Boolean>(key, value);
+  {$IFNDEF FPC}
+  SetValue<Boolean>(Key, Value);
+  {$ELSE}
+  SetValue(Key, Value);
+  {$ENDIF}
+end;
+
+procedure TLocalStorage4Pascal.SetValue(const Key: string; const Value: Int64);
+begin
+  {$IFNDEF FPC}
+  SetValue<Int64>(Key, Value);
+  {$ELSE}
+  SetValue(Key, Value);
+  {$ENDIF}
 end;
 
 end.
-
